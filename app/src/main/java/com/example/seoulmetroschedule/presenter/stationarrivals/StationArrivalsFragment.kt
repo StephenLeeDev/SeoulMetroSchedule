55b92d954482e2b6ab5d994ef4a05054cdd1cfa8
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-10 12:00:15 +0900

    Add | Generate Stations & StationArrivals fragments

---
+++ app/src/main/java/com/example/seoulmetroschedule/presenter/stationarrivals/StationArrivalsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.seoulmetroschedule.presenter.stationarrivals
+
+import androidx.fragment.app.Fragment
+
+class StationArrivalsFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/example/seoulmetroschedule/presenter/stations/StationsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.seoulmetroschedule.presenter.stations
+
+import androidx.fragment.app.Fragment
+
+class StationsFragment : Fragment() {
+
+
+}
