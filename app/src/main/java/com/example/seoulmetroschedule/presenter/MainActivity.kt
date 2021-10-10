66b58289030a23fae303f1f767b94198e979b113
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-10 11:59:10 +0900

    Move | Move MainActivity package location

---
+++ app/src/main/java/com/example/seoulmetroschedule/presenter/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.seoulmetroschedule.presenter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.seoulmetroschedule.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
